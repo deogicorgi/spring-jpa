a2ea7ee04a80cd0dcbc73ac414b14d26161e92e1
Wondeok Kang <deogicorgi@gmail.com>
2020-09-05 21:07:24 +0900

    Create entities.

---
+++ src/main/java/io/github/deogicorgi/spring/jpa/entity/Member.java
@@ -0,0 +1,23 @@
+package io.github.deogicorgi.spring.jpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "'MEMBER'")
+public class Member {
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String username;
+    private Integer age;
+}
