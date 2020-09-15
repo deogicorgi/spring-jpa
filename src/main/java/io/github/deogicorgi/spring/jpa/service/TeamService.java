e73d8e198b9c62489a1d871aa82154344dd95830
Wondeok Kang <deogicorgi@gmail.com>
2020-09-15 20:32:25 +0900

    Team 서비스 생성

--- src/main/java/io/github/deogicorgi/spring/jpa/service/TeamService.java
+++ src/main/java/io/github/deogicorgi/spring/jpa/service/TeamService.java
@@ -1,4 +1,28 @@
 package io.github.deogicorgi.spring.jpa.service;
 
+import io.github.deogicorgi.spring.jpa.entity.Team;
+import io.github.deogicorgi.spring.jpa.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class TeamService {
+    
+    private final TeamRepository teamRepository;
+
+    public Team save(Team team) {
+        return teamRepository.save(team);
+    }
+
+    public Team findById(Integer id) {
+        Optional<Team> team = teamRepository.findById(id);
+        return team.orElseThrow();
+    }
+
+    public void remove(Team team){
+        teamRepository.delete(team);
+    }
 }
