669f8a1406d63150836d2329542669b75dce68fe
Wondeok Kang <deogicorgi@gmail.com>
2020-09-16 22:11:37 +0900

    멤버 클래스 필드 추가

--- src/main/java/io/github/deogicorgi/spring/jpa/entity/Member.java
+++ src/main/java/io/github/deogicorgi/spring/jpa/entity/Member.java
@@ -1,20 +1,64 @@
 package io.github.deogicorgi.spring.jpa.entity;
 
+import io.github.deogicorgi.spring.jpa.entity.enums.Gender;
+import io.github.deogicorgi.spring.jpa.entity.enums.converter.BirthdayConverter;
+import io.github.deogicorgi.spring.jpa.entity.enums.converter.GenderEnumConverter;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
+/**
+ * Class with main content for members
+ */
 @Data
 @Entity
 @Table(name = "member")
 public class Member {
+    /**
+     * ID of the member.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Real name of the member.
+     */
     @Column(nullable = false, length = 100)
-    private String name;
+    private String realName;
 
+    /**
+     * Stage name of the member.
+     */
+    @Column(length = 100)
+    private String stageName;
+
+    /**
+     * Gender of the member.
+     *
+     * @see Gender
+     */
+    @Column(nullable = false)
+    @Convert(converter = GenderEnumConverter.class)
+    private Gender gender;
+
+    /**
+     * Age of the member.
+     */
+    @Column(nullable = false)
+    private Integer age;
+
+    /**
+     * Birthday of member.
+     */
+    @Column(nullable = false, length = 40)
+    @Convert(converter = BirthdayConverter.class)
+    private LocalDate birthday;
+
+    /**
+     * Member's team
+     */
     @ManyToOne
     @JoinColumn(name = "team_id", nullable = false)
     private Team team;
